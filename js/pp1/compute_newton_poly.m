f9f6e09a8aacbbf53ca6c2d04d91fc7cfdcf3a74
John <mtasa.lua@gmx.de>
2015-12-20 17:08:21 +0100

    - added compute-newton_poly and divdiff

--- js/pp1/compute_newton_poly.m
+++ js/pp1/compute_newton_poly.m
@@ -1,3 +1,8 @@
-function [ci,xi] = compute_newton_poly (xi,yi)
+% Eingabe: x...Stutzstellen   [x0,x1,...,xn]
+%          y...Stutzwerte     [y0,y1,...,yn]
+% Ausgabe: c...Koeffizienten  [c0,c1,...,cn]
+% Indizierung: x(1)=x0, x(2)=x1, usw
 
+function [ci,xi] = compute_newton_poly (xi,yi)
+  [ci,xi]=[divdiff(xi,yi),xi];
 endfunction
