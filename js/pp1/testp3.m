00aa96eb401f04bf49b805e601005658bf675ea7
John <mtasa.lua@gmx.de>
2015-12-22 12:45:27 +0100

    - restructured, advances in ex3

--- js/pp1/testp3.m
+++ js/pp1/testp3.m
@@ -1,21 +1,36 @@
 % Teilaufgabe (a)
 
-f=@(x)exp(x);
-n=1;
-m=10;
-x=zeros(n+1,m+1);
+% Ausgangsdaten definieren
+f=@(x) exp(x);
+n=1; k = 0:10; h = 2.^-k'; j = k.+1;
+x = zeros(m+1,n+1);
+i = 0:n;
 
-k=0:10;
-h=2.^(-k);
-
-% i+1. Zeile = xi
-for i=0:n
-  x(i+1,:)=i.*h;
+% x enthaelt in der j-ten zeile die Stuetzstellen zu h(j)
+for l = j
+  x(l,:) = i.*h(l);
 endfor
 
+% Funktionswerte berechnen
 y=f(x);
-% Da x(1,:)=zeros(1,11), sind hier keine geeigneten Stuetzstellen gegeben, weil verschiedene Stuetzstellen nicht gleich sein duerfen.
-%[c0,x0]=compute_newton_poly(x(1,:),y(1,:));
 
-% c1 enthält nun die Koeffizienten von p_1 in Newon-Darstellung
-[c1,x1]=compute_newton_poly(x(2,:),y(2,:))
+% Die Interpolationspolynome zu den Stuetzstellen xi haben die Newton-Koeffizienten ci.
+for l = j
+  [ci,xi] = compute_newton_poly(x(l,:),y(l,:));
+endfor
+
+% Teilaufgabe (b)
+
+% e_h enthält in i-ten Eintrag eine Naeherung für den Interpolationsfehler, wenn f mit h(i) angenaehert wird
+e_h = j';
+for l = j;
+  % das Gitter
+  G = 0:h(l)/10:n*h(l);
+  % die Funktionswerte beim Gitter
+  fG = f(G);
+  % die Approximierten Werte beim Gitter
+  fGA = eval_newton_poly(ci,xi,G);
+  % der maximale Fehler
+  e_h(l) = norm(fG-fGA, inf);
+endfor
+e_h
