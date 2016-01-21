c6fe91310740f5fd92828faf02ad5bb0d5c21933
John <mtasa.lua@gmx.de>
2016-01-21 13:49:35 +0100

    - started

---
+++ js/pp5/bisection.m
@@ -0,0 +1,20 @@
+% Eingabe: f ... function handle
+% a, b ... Intervallgrenzen
+% tol ... Schranke fuer Abbruch
+% Ausgabe: x ... Naeherung fuer Loesung von f(x)=0
+% xk ... Vektor der Iterierten
+function [x,xk]=bisection(f,a,b,tol)
+  fa=f(a); fb=f(b); xk=[];
+  while 1
+    c=(a+b)/2; fc=f(c);
+    xk=[xk,c];
+    if abs(b-a)<=tol
+      break;
+    end
+    if fc*fa<0
+      b=c; fb=fc;
+    else
+      a=c; fa=fc;
+    end
+  end
+endfunction
