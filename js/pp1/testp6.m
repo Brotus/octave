d103d19c16262cf4f8e2bcdb3c0d83d077dedef3
John <mtasa.lua@gmx.de>
2016-01-25 20:41:44 +0100

    - testp6 stinkt noch

--- js/pp1/testp6.m
+++ js/pp1/testp6.m
@@ -11,22 +11,34 @@ function l=lagrange(x,xi,j)
   end
 end
 
-for j=0:5
-	for i=0:5
-		% Berechne das j-te Polynom an allen i
-		y(j+1,i+1) = lagrange(i,xi,j);
-	end
-	% Speicher die Werte zum Ploten
-	newton(j+1,:) = compute_newton_poly(xi,y(j+1,:));
+y=eye(6);
+for i=1:6
+  [ci,xi]=compute_newton_poly(xi,y(:,i));
+  y(:,i)=ci;
 end
-
 y
-newton
 
-plot(z,eval_newton_poly(newton(1,1:6),newton(1,7:12),z),'r',
-z,eval_newton_poly(newton(2,1:6),newton(2,7:12),z),'b',
-z,eval_newton_poly(newton(3,1:6),newton(3,7:12),z),'g',
-z,eval_newton_poly(newton(4,1:6),newton(4,7:12),z),'c',
-z,eval_newton_poly(newton(5,1:6),newton(5,7:12),z),'m',
-z,eval_newton_poly(newton(6,1:6),newton(6,7:12),z),'k')
-legend('l_0(x)', 'l_1(x)', 'l_2(x)', 'l_3(x)', 'l_4(x)', 'l_5(x)', "Location", 'northwest')
+%plot(z,eval_newton_poly(z,xi,y(:,1)))
+
+
+
+
+%for j=0:5
+%	for i=0:5
+%		% Berechne das j-te Polynom an allen i
+%		y(j+1,i+1) = lagrange(i,xi,j);
+%	end
+%	% Speicher die Werte zum Ploten
+%	newton(j+1,:) = compute_newton_poly(xi,y(j+1,:));
+%end
+
+%y
+%newton
+
+%plot(z,eval_newton_poly(newton(1,1:6),newton(1,7:12),z),'r',
+%z,eval_newton_poly(newton(2,1:6),newton(2,7:12),z),'b',
+%z,eval_newton_poly(newton(3,1:6),newton(3,7:12),z),'g',
+%z,eval_newton_poly(newton(4,1:6),newton(4,7:12),z),'c',
+%z,eval_newton_poly(newton(5,1:6),newton(5,7:12),z),'m',
+%z,eval_newton_poly(newton(6,1:6),newton(6,7:12),z),'k')
+%legend('l_0(x)', 'l_1(x)', 'l_2(x)', 'l_3(x)', 'l_4(x)', 'l_5(x)', "Location", 'northwest')
