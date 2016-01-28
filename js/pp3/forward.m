bc3e36f67ae70ef8a7619c5a096584250386433b
John <mtasa.lua@gmx.de>
2016-01-28 10:31:10 +0100

    - added forward Alternative and solveCholesky

--- js/pp3/forward.m
+++ js/pp3/forward.m
@@ -1,15 +1,15 @@
 % linke OBERE dreiecksmatrix
 
 % Implementierung des  Forwaertseinsetzens mit mehreren rechten Seiten
-% Eingabe: gauss-transformierte Matrix R
+% Eingabe: linke obere Dreiecksmatrix L
 %		   die rechten Seiten y
 % Ausgabe: eine Matrix x, die in jeder Spalte das Resultat des Forwaertseinsetzens
 %				der jeweiligen Spalte aus y enthaelt
-function x = forward (R, y)
-  n = size(R,1); x=0.*y;
+function x = forward (L, y)
+  n = size(L,1); x=0.*y;
   for i = n:-1:1
     for j = 1:(n-i)
-      y(i,:)=y(i,:)-R(i,j).*x(j,:);
+      y(i,:)=y(i,:)-L(i,j).*x(j,:);
     end
-    x(n-i+1,:)=y(i,:)/R(i,n-i+1);
+    x(n-i+1,:)=y(i,:)/L(i,n-i+1);
   end
---
+++ js/pp3/solveCholesky.m
@@ -0,0 +1,16 @@
+
+% Ax=b iff LDL'x=b:
+% y1=forward(L,b);  (Ly1=b, DL'x=y1)
+% y2=diagsolve(D,y1); (Dy2=y1)
+% x=backward(L',y2);  (L'x=y2)
+function x=solveCholesky(A,b)
+  [L,D]=cholesky(A);
+  y1=forwaerts(L,b);
+  d=diag(D);
+  y2=y1;
+  for i=1:size(y1,1)
+    y2(i,:)=y1(i,:)./d(i);
+  end
+  x=backward(L',y2);
+  
+  
