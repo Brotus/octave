2f52e4428aa72e1ef37755b005215e10ccefc7de
John <mtasa.lua@gmx.de>
2016-01-29 10:55:04 +0100

    - tests

--- js/pp3/solveCholesky.m
+++ js/pp3/solveCholesky.m
@@ -5,12 +5,14 @@
 % x=backward(L',y2);  (L'x=y2)
 function x=solveCholesky(A,b)
   [L,D]=cholesky(A);
-  y1=forwaerts(L,b);
+  y1=forwardAltern(L,b);
+  
   d=diag(D);
   y2=y1;
   for i=1:size(y1,1)
     y2(i,:)=y1(i,:)./d(i);
   end
+  
   x=backward(L',y2);
   
   
