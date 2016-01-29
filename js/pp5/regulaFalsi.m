a6a45a80dd42f2a6e4cddcc6c4c02f9a10495ac4
John <mtasa.lua@gmx.de>
2016-01-29 11:19:10 +0100

    - implemented regulafalsi

--- js/pp5/regulaFalsi.m
+++ js/pp5/regulaFalsi.m
@@ -1,3 +1,17 @@
 function [x,xk] = regulaFalsi (f,a,b,tol,maxit)
-
-
+  xk=[];
+  for i=1:maxit
+    fa=f(a); fb=f(b);
+    c=(a*f(b)-b*f(a))/(f(b)-f(a));
+    fc=f(c);
+    xk=[xk,c];
+    if norm(fc)<tol
+      x=c;
+      break;
+    end
+    if fa*fc>0
+      a=c;
+    else %fb*fc>0
+      b=c;
+    end
+  end
