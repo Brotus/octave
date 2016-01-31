5e3aa49d792c7e2f14a6825113621708edcde8d5
John <mtasa.lua@gmx.de>
2016-01-31 21:57:55 +0100

    - cholesky

--- js/pp3/choleskytest.m
+++ js/pp3/choleskytest.m
@@ -1,15 +1,15 @@
-function [L,D] = cholesky (A)
+function[L,D]=choleskytest(A)
   n=size(A,1); D=zeros(n,n); L=eye(n); dlj = zeros(n,n);
-  for k=1:n
-    
+  for k=1:n 
     for j=1:k-1
       dlj(k,j)=D(j,j)*L(k,j);
-      A(i,k)=A(i,k)-L(i,j)*dlj(k,j);
+      A(k,k)=A(k,k)-L(k,j)*dlj(k,j);
     end
     D(k,k)=A(k,k);
+    
     for i=k+1:n
       for j=1:k-1
-        A(k,k)=A(k,k)-L(k,j)*dlj(k,j);
+        A(i,k)=A(i,k)-L(i,j)*dlj(k,j);
       end
       L(i,k)=A(i,k)/D(k,k);
     end
