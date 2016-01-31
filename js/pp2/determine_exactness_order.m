85c17883bd6f2e0f91807a862b91218112cb80d1
John <mtasa.lua@gmx.de>
2016-01-31 22:06:05 +0100

    - wtf

--- js/pp2/determine_exactness_order.m
+++ js/pp2/determine_exactness_order.m
@@ -3,16 +3,14 @@
 %             wi ist ein Vektor der Integrationsgewichte
 % Ausgabe:    m ist der Exaktheitsgrad der Quadraturformel
 function m = determine_exactness_order (ti,wi)
-  eps = 1e-5;
+  eps = 1e-14;
   a = 0; b = 1;
   
   m = 0;
   while 1
     f = @(x) x^m;
 	% int_0^1 x^m dx = 1/(m+1)
-    p = quadrature(f,0,1,ti,wi);
-    q = 1/(m+1);
-    err = abs(p-q);
+    err = abs(quadrature(f,0,1,ti,wi)-1/(m+1));
     if (err >= eps)
       break;
     else
