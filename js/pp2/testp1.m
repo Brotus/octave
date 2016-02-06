2c524cc18f01693b6fc78c331380d8ca66252c34
John <mtasa.lua@gmx.de>
2016-02-06 17:22:41 +0100

    - better pp1

--- js/pp2/testp1.m
+++ js/pp2/testp1.m
@@ -1,4 +1,3 @@
-format short G;
 f=@(x)exp(x); a=0; n=5;
 h = 2.^-(0:n);
 r = zeros(3,n);
@@ -14,27 +13,24 @@ for i = 1:(n+1)
   r(3,i) = quadrature(f,a,b,[0,1/2,1],[1/6,4/6,1/6]);
 end
 
-r
-
-% h | rechteck | rechteck eoc | trapez | trapez eoc | simpson | simpson eoc
-% -------------------------------------------------------------------------
-% 1 |
-% 0.5
+disp(['rec','       ','tra','       ','sim'])
+disp(num2str(r',"%10f"))
 
 tab = zeros(n+1,7);
 tab(:,1)=h';
 
 for i = 1:3
   for j = 1:(n+1)
-    %tab(j,2*i)=abs(r(i,j)-quad(f,a,h(j)));
 		tab(j,2*i)=abs(r(i,j)-f(h(j))+f(a));
     if (j > 1)
-      %tab(j,2*i+1)=log(r(i,j)/r(i,j-1))/log(h(j)/h(j-1));
 			tab(j,2*i+1)=log(tab(j-1,2*i)/tab(j,2*i))/log(h(j-1)/h(j));
     else
       tab(j,2*i+1)=-1;
     endif
   endfor
 endfor
-
-tab
+disp('')
+disp('fehler & eoc')
+disp('eoc == -1 means undefined')
+disp(['h','         ','rec','       ','rec eoc','   ','tra','       ','tra eoc','   ','sim','       ','sim eoc'])
+disp(num2str(tab,"%10f"))
