b0fbbb267e34687290906b53a059748f93bb96b6
PatrickKorus <korus@gmx.net>
2016-02-07 16:21:04 +0100

    - now on matlab

--- pk/pp2/quadrature.m
+++ pk/pp2/quadrature.m
@@ -1,6 +1,12 @@
 function r = quadrature (f,a,b,ti,wi)
-r= 0;
-for i=1:length(wi);
-r = r + wi(i)*f(a+ti(i)*(b-a));
-end
-r=(b-a)*r
+% Eingabe:    f...function handle
+%             a...linke Grenze
+%             b...rechte Grenze
+%             ti...Stützstellen
+%             wi...Integrationsgewichte
+% Ausgabe     r...Wert der Quadratur
+  r= 0;
+  for i=1:length(wi);
+    r = r + wi(i)*f(a+ti(i)*(b-a));
+  end
+  r=(b-a)*r;
--- pk/pp2/testp1.m
+++ pk/pp2/testp1.m
@@ -2,24 +2,48 @@
 f=@(x) exp(x);
 a=0;
 b=[2.^-[0:5]];
+q = zeros(3,6);
 
-% Rechtsseitige Rechtecksregel
-fprintf("Linksseit \n")
+% Linksseitige Rechtecksregel
 ti = 0; wi = 1;
 for i=1:6;
-quadrature (f,a,b(i),ti,wi);
+q(1,i) = quadrature (f,a,b(i),ti,wi);
 end
 
 % Trapezregel
-fprintf("Trapezregel \n")
 ti = [0,1]; wi =[1/2 1/2];
 for i=1:6;
-quadrature (f,a,b(i),ti,wi);
+q(2,i) = quadrature (f,a,b(i),ti,wi);
 end
 
 % Simpson
-fprintf("Simpson \n")
 ti = [0 0.5 1]; wi = [1/6 4/6 1/6];
 for i=1:6;
-quadrature (f,a,b(i),ti,wi);
+q(3,i) = quadrature (f,a,b(i),ti,wi);
+end
+
+% Die exakten Werte
+integ = [exp(1)-1,exp(1/2)-1,exp(1/4)-1,exp(1/8)-1,exp(1/16)-1,exp(1/32)-1];
+Tab = zeros(6,4);
+Tab(:,1) = [0:5];
+for k = 1:3;
+  if (k == 1)
+      fprintf('Linksseitige Rechtecksregel \n')
+    elseif (k == 2)
+      fprintf('Trapezregel \n')
+    else
+      fprintf('Simpson \n')
+  end
+    fprintf('h | Quadtratur | Fehler | eoc')
+  for i = 1:6; 
+    Tab(i,2) = q(k,i);
+    Tab(i,3) = abs(integ(i)-q(k,i));
+    if (i > 1)
+    % log fehlerquotient / log h-quotient
+      Tab(i,4) = log(Tab(i-1,3)/Tab(i,3))/log(i/(i-1));
+    else 
+      Tab(i,4) = inf;  
+    end
+  end
+  Tab
 end
