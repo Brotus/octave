20359c6df9541f950d0522fc3412c065a8e7583e
John <mtasa.lua@gmx.de>
2016-02-09 18:15:45 +0100

    - pp3 final commit

--- js/pp3/testp1.m
+++ js/pp3/testp1.m
@@ -1,6 +1,6 @@
-clc
+clc;
 
-disp('Teilaufgabe (a)')
+disp('Teilaufgabe (a)');
 A=[2,1;1,2]; b=[3,4;3,1];
 [R,y]=gauss(A,b);
 disp('R');
@@ -16,7 +16,7 @@ disp(y);
 % Zustand ist. (-1)^0=1.
 
 
-disp('Teilaufgabe (b)')
+disp('Teilaufgabe (b)');
 A=[0,1;1,1]; b=[1:10;2:11];
 [R,y]=gauss(A,b);
 disp('R');
@@ -31,7 +31,7 @@ disp('y, b=[]');
 disp(y);
 % Die beiden Zeilen müssen ein mal vertauscht werden. Dann hat A Zeilenstufenform. (-1)^1=-1
 
-disp('Teilaufgabe (c)')
+disp('Teilaufgabe (c)');
 A=-ones(10)+11*eye(10); b=(1:10)';
 [R,y]=gauss(A,b);
 disp('R');
